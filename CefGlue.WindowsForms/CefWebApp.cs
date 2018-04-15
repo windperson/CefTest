fc883c4e2852a8b2e7af87245af231f72fe9c7c1
Chen Yu Pao <windperson@gmail.com>
2018-04-15 14:59:54 +0800

    buildable CEF

---
+++ CefGlue.WindowsForms/CefWebApp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Xilium.CefGlue.WindowsForms
+{
+	/* BEG: erinus */
+	public class CefWebApp : CefApp
+	{
+		private readonly CefWebBrowserProcessHandler _browserProcessHandler;
+		private readonly CefWebRenderProcessHandler _renderProcessHandler;
+
+		public CefWebApp(CefWebBrowser browser)
+		{
+			_browserProcessHandler = new CefWebBrowserProcessHandler(browser);
+			_renderProcessHandler = new CefWebRenderProcessHandler(browser);
+		}
+
+		protected override CefBrowserProcessHandler GetBrowserProcessHandler()
+		{
+			return _browserProcessHandler;
+		}
+
+		protected override CefRenderProcessHandler GetRenderProcessHandler()
+		{
+			return _renderProcessHandler;
+		}
+
+		protected override CefResourceBundleHandler GetResourceBundleHandler()
+		{
+			return base.GetResourceBundleHandler();
+		}
+
+		protected override void OnBeforeCommandLineProcessing(string processType, CefCommandLine commandLine)
+		{
+			base.OnBeforeCommandLineProcessing(processType, commandLine);
+
+			//commandLine.AppendSwitch("disable-gpu-shader-disk-cache");
+		}
+
+		protected override void OnRegisterCustomSchemes(CefSchemeRegistrar registrar)
+		{
+			base.OnRegisterCustomSchemes(registrar);
+		}
+	}
+	/* END: erinus */
+}
--- CefTest/Form1.Designer.cs
+++ CefTest/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cefWebBrowser1 = new Xilium.CefGlue.WindowsForms.CefWebBrowser();
+            this.SuspendLayout();
+            // 
+            // cefWebBrowser1
+            // 
+            this.cefWebBrowser1.BrowserSettings = null;
+            this.cefWebBrowser1.Location = new System.Drawing.Point(13, 13);
+            this.cefWebBrowser1.Name = "cefWebBrowser1";
+            this.cefWebBrowser1.Size = new System.Drawing.Size(775, 425);
+            this.cefWebBrowser1.TabIndex = 0;
+            this.cefWebBrowser1.Text = "cefWebBrowser1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cefWebBrowser1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Xilium.CefGlue.WindowsForms.CefWebBrowser cefWebBrowser1;
     }
 }
 
